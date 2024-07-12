d1ffb09766b139a8867a9bc7d73fa9a8aca29a1a
Lev Bakin <lev@onapproval.com>
2024-07-12 15:29:25 -0700

    feat: questionnaire steps now have choice options

--- components/Questionnaire.js
+++ components/Questionnaire.js
@@ -3,15 +3,28 @@ import Step from './Step';
 
 const Questionnaire = ({ isOpen, onClose }) => {
   const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [formData, setFormData] = useState({ 
+    name: '', 
+    email: '', 
+    message: '',
+    websiteTimeline: '',
+    budget: ''   
+  });
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const totalSteps = 3;
+  const totalSteps = 5;
 
   const nextStep = () => setStep(step + 1);
   const prevStep = () => setStep(step - 1);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleOptionChange = (name, value) => {
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
@@ -57,8 +70,110 @@ const Questionnaire = ({ isOpen, onClose }) => {
           X
         </button>
         <form onSubmit={handleSubmit} className="space-y-4">
-          {step === 1 && (
+        {step === 1 && (
             <Step stepNumber={step} totalSteps={totalSteps} onNext={nextStep}>
+              <h3 className="text-lg mb-4 text-black">When do you need your website?</h3>
+              <div className="space-y-2 text-black text-left">
+              <label className="block">
+                  <input 
+                    type="radio" 
+                    name="websiteTimeline" 
+                    value="As soon as possible" 
+                    checked={formData.websiteTimeline === 'As soon as possible'}
+                    onChange={() => handleOptionChange('websiteTimeline', 'As soon as possible')}
+                    className="mr-2"
+                  />
+                  As soon as possible
+                </label>
+                <label className="block">
+                  <input 
+                    type="radio" 
+                    name="websiteTimeline" 
+                    value="In the next 2 months" 
+                    checked={formData.websiteTimeline === 'In the next 2 months'}
+                    onChange={() => handleOptionChange('websiteTimeline', 'In the next 2 months')}
+                    className="mr-2"
+                  />
+                  In the next 2 months
+                </label>
+                <label className="block">
+                  <input 
+                    type="radio" 
+                    name="websiteTimeline" 
+                    value="Within 6 months" 
+                    checked={formData.websiteTimeline === 'Within 6 months'}
+                    onChange={() => handleOptionChange('websiteTimeline', 'Within 6 months')}
+                    className="mr-2"
+                  />
+                  Within 6 months
+                </label>
+                <label className="block">
+                  <input 
+                    type="radio" 
+                    name="websiteTimeline" 
+                    value="There's no rush" 
+                    checked={formData.websiteTimeline === "There's no rush"}
+                    onChange={() => handleOptionChange('websiteTimeline', "There's no rush")}
+                    className="mr-2"
+                  />
+                  There's no rush
+                </label>
+              </div>
+            </Step>
+          )}
+          {step === 2 && (
+            <Step stepNumber={step} totalSteps={totalSteps} onNext={nextStep} onPrevious={prevStep}>
+              <h3 className="text-lg mb-4 text-black">What is your budget?</h3>
+              <div className="space-y-2 text-black text-left">
+                <label className="block">
+                  <input 
+                    type="radio" 
+                    name="budget" 
+                    value="$500" 
+                    checked={formData.budget === '$500'}
+                    onChange={() => handleOptionChange('budget', '$500')}
+                    className="mr-2"
+                  />
+                  $500
+                </label>
+                <label className="block">
+                  <input 
+                    type="radio" 
+                    name="budget" 
+                    value="$1000" 
+                    checked={formData.budget === '$1000'}
+                    onChange={() => handleOptionChange('budget', '$1000')}
+                    className="mr-2"
+                  />
+                  $1000
+                </label>
+                <label className="block">
+                  <input 
+                    type="radio" 
+                    name="budget" 
+                    value="$1500" 
+                    checked={formData.budget === '$1500'}
+                    onChange={() => handleOptionChange('budget', '$1500')}
+                    className="mr-2"
+                  />
+                  $1500
+                </label>
+                <label className="block">
+                  <input 
+                    type="radio" 
+                    name="budget" 
+                    value="$2000+" 
+                    checked={formData.budget === '$2000+'}
+                    onChange={() => handleOptionChange('budget', '$2000+')}
+                    className="mr-2"
+                  />
+                  $2000+
+                </label>
+              </div>
+            </Step>
+          )}
+          {step === 3 && (
+            <Step stepNumber={step} totalSteps={totalSteps} onNext={nextStep} onPrevious={prevStep}>
               <input 
                 type="text" 
                 name="name" 
@@ -70,7 +185,7 @@ const Questionnaire = ({ isOpen, onClose }) => {
               />
             </Step>
           )}
-          {step === 2 && (
+          {step === 4 && (
             <Step stepNumber={step} totalSteps={totalSteps} onNext={nextStep} onPrevious={prevStep}>
               <input 
                 type="email" 
@@ -83,19 +198,19 @@ const Questionnaire = ({ isOpen, onClose }) => {
               />
             </Step>
           )}
-          {step === 3 && (
+          {step === 5 && (
             <Step stepNumber={step} totalSteps={totalSteps} onPrevious={prevStep}>
               <textarea 
                 name="message" 
                 value={formData.message} 
                 onChange={handleChange} 
-                placeholder="Message" 
+                placeholder="Tell me a little about your project!" 
                 className="w-full p-2 border rounded mt-2 text-black" 
                 required 
               />
             </Step>
           )}
-          {step === 4 && (
+          {step === 6 && (
             <div className="text-center">
               <h2 className="text-xl text-black font-semibold mb-4">Thank you for your request. I'm excited to start this project together!</h2>
               <button onClick={onClose} className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">Close</button>
