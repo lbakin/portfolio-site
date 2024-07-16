3b337647156fffefd32736c97244cd8463bbccfe
Lev Bakin <lev@onapproval.com>
2024-07-16 15:10:22 -0700

    chore: added favicon and optimized images for size

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel="icon" href="/LB_fav.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Kaushan+Script:wght@200;300;400;500;600;700&display=swap"
           rel="stylesheet"
