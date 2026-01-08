40facec9f14370c00b5e0c89d06658552e81dc3e
Lev <53977554+lbakin@users.noreply.github.com>
2026-01-07 17:55:55 -0800

    Update Showcase.js
    quick little text change

--- components/Showcase.js
+++ components/Showcase.js
@@ -53,7 +53,7 @@ const Showcase = () => {
     <div className={`flex flex-col xl:flex-row justify-center items-center h-auto xl:h-screen transition-all duration-1000 ease-in-out ${isHovered ? 'hover-gradient' : ''}`}>
       <div className="w-full xl:w-2/5 p-8 relative text-center xl:text-left mt-8 xl:mt-0">
         <p className="text-2xl sm:text-3xl md:text-4xl font-sans leading-relaxed tracking-wide">
-        Hi, I'm Lev Bakin. I create websites that blend <span className="flair-word">visual</span> appeal with seamless usability. 
+        Hi, I'm Lev Bakin. I create websites that blend <span className="flair-word">visual</span> appeal with smooth usability. 
         I specialize in quickly building small portfolio sites that highlight your unique work and personal brand.
         </p>
       </div>
